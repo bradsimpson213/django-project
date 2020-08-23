833af0fd4bc95244a3aff3709f820539838f2a07
Brad Simpson <bradsimpson@icloud.com>
2020-08-23 00:22:50 -0400

    set up thumbnails

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    #add in thumbnail
+    thumb = models.ImageField(default='default.png', blank=True)
     #add in author
 
     def __str__(self):
