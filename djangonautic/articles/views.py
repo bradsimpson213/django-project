13c264c601b6443776492b31cbc699da97f8a2a2
Brad Simpson <bradsimpson@icloud.com>
2020-08-25 00:05:09 -0400

    set up next on create

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.contrib.auth.decorators import login_required
 
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, "articles/article_list.html", { 'articles': articles })
 
+
 def article_detail(request, slug):
-    # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, "articles/article_detail.html", {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, "articles/article_create.html")
