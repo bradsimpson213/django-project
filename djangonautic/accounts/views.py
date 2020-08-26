a688789026420f2ab77f4e1c644d6ada97fb0ac1
Brad Simpson <bradsimpson@icloud.com>
2020-08-25 23:18:53 -0400

    navbar setup

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -18,7 +18,7 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            user = form.get_user
+            user = form.get_user()
             login(request, user)
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
